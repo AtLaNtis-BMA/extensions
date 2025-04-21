718c6b9d89f04ede7a35370f54a218aa0901a7ab
AtLaNtis-BMA <68110540+AtLaNtis-BMA@users.noreply.github.com>
2025-04-21 14:50:41 +0200

    Delete popup.js

--- popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('redirectButton').addEventListener('click', function() {
-    chrome.tabs.create({ url: "https://support.microsoft.com/fr-fr/topic/installation-de-microsoft-defender-ba7d17d7-464b-444d-bb47-990b7278369d" });
-  });
-  
